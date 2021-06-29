e6391dd886b4f361fbe720525bea5be0085a04ea
pradyuman-verma <pradyumn_verma27@users.noreply.github.com>
2021-06-29 17:12:00 +0530

    update

--- B/B_Array_Stabilization.py
+++
@@ -1,8 +0,0 @@
-def ans(arr):
-    arr.sort()
-    return(min(arr[-2] - arr[0], arr[-1] - arr[1]))
-
-if __name__ == '__main__':
-    n = int(input())
-    a = list(map(int, input().strip().split()))
-    print(ans(a))
--- B/B_Nastya_Is_Playing_Computer_Games.py
+++
@@ -1,8 +0,0 @@
-def ans(n, k):
-    if(n == 2):
-        return(6)
-    return(min(k - 1, n - k) + 6 + 3 *(n - 2))
-
-if __name__ == '__main__':
-    nk = list(map(int, input().strip().split()))
-    print(ans(nk[0], nk[1]))
